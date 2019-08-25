505e489a726f38bfcc55e3052b556a210f9d3222
Daniel Mentiplay <d.mentiplay@gmail.com>
2019-08-25 13:24:31 +1000

    Add read JSON and dict in tests

--- phantomconfig/tests/stub/test_data.py
+++ phantomconfig/tests/stub/test_data.py
@@ -4,6 +4,10 @@ import datetime
 
 from phantomconfig.phantomconfig import ConfigVariable
 
+test_datetime = datetime.datetime.strptime(
+    '01/01/1999 12:00:00.0', '%d/%m/%Y %H:%M:%S.%f'
+)
+
 test_header = [
     'Runtime options file for Phantom, written 01/01/1999 12:00:00.0',
     'Options not present assume their default values',
@@ -194,4 +198,151 @@ test_config = {
         'cut the drag on the gas phase (0=no, 1=yes)',
         'options controlling dust',
     ),
+}
+
+test_dict = {
+    '__header__': test_header,
+    '__datetime__': test_datetime,
+    'logfile': ['test01.log', 'file to which output is directed', 'job name'],
+    'dumpfile': ['test_00000', 'dump file to start from', 'job name'],
+    'tmax': [100.0, 'end time', 'options controlling run time and input/output'],
+    'dtmax': [
+        1.000,
+        'time between dumps',
+        'options controlling run time and input/output',
+    ],
+    'nmax': [
+        -1,
+        'maximum number of timesteps (0=just get derivs and stop)',
+        'options controlling run time and input/output',
+    ],
+    'nout': [
+        -1,
+        'number of steps between dumps (-ve=ignore)',
+        'options controlling run time and input/output',
+    ],
+    'nmaxdumps': [
+        -1,
+        'stop after n full dumps (-ve=ignore)',
+        'options controlling run time and input/output',
+    ],
+    'twallmax': [
+        datetime.timedelta(0),
+        'maximum wall time (hhh:mm, 000:00=ignore)',
+        'options controlling run time and input/output',
+    ],
+    'dtwallmax': [
+        datetime.timedelta(0),
+        'maximum wall time between dumps (hhh:mm, 000:00=ignore)',
+        'options controlling run time and input/output',
+    ],
+    'nfulldump': [
+        10,
+        'full dump every n dumps',
+        'options controlling run time and input/output',
+    ],
+    'iverbose': [
+        0,
+        'verboseness of log (-1=quiet 0=default 1=allsteps 2=debug 5=max)',
+        'options controlling run time and input/output',
+    ],
+    'C_cour': [0.300, 'Courant number', 'options controlling accuracy'],
+    'C_force': [0.250, 'dt_force number', 'options controlling accuracy'],
+    'tolv': [
+        1.000e-02,
+        'tolerance on v iterations in timestepping',
+        'options controlling accuracy',
+    ],
+    'hfact': [
+        1.000,
+        'h in units of particle spacing [h = hfact(m/rho)^(1/3)]',
+        'options controlling accuracy',
+    ],
+    'tolh': [
+        1.000e-04,
+        'tolerance on h-rho iterations',
+        'options controlling accuracy',
+    ],
+    'restartonshortest': [
+        False,
+        'restart with all particles on shortest timestep',
+        'options controlling accuracy',
+    ],
+    'alpha': [
+        0.100,
+        'art. viscosity parameter',
+        'options controlling hydrodynamics, artificial dissipation',
+    ],
+    'beta': [
+        2.000,
+        'beta viscosity',
+        'options controlling hydrodynamics, artificial dissipation',
+    ],
+    'avdecayconst': [
+        0.100,
+        'decay time constant for viscosity switches',
+        'options controlling hydrodynamics, artificial dissipation',
+    ],
+    'damp': [
+        0.000,
+        'artificial damping of velocities (if on, v=0 initially)',
+        'options controlling hydrodynamics, artificial dissipation',
+    ],
+    'ieos': [
+        3,
+        'eqn of state (1=isoth;2=adiab;3=locally iso;8=barotropic)',
+        'options controlling equation of state',
+    ],
+    'mu': [2.381, 'mean molecular weight', 'options controlling equation of state'],
+    'h_soft_sinksink': [
+        0.000,
+        'softening length between sink particles',
+        'options controlling sink particles',
+    ],
+    'f_acc': [
+        0.800,
+        'particles < f_acc*h_acc accreted without checks',
+        'options controlling sink particles',
+    ],
+    'iexternalforce': [
+        0,
+        '1=star,2=coro,3=bina,4=prdr,5=toru,6=toys,7=exte,8=spir,9=Lens,10=neut,11=Eins,',
+        'options relating to external forces',
+    ],
+    'irealvisc': [
+        0,
+        'physical viscosity type (0=none,1=const,2=Shakura/Sunyaev)',
+        'options controlling physical viscosity',
+    ],
+    'shearparam': [
+        0.100,
+        'magnitude of shear viscosity (irealvisc=1) or alpha_SS (irealvisc=2)',
+        'options controlling physical viscosity',
+    ],
+    'bulkvisc': [
+        0.000,
+        'magnitude of bulk viscosity',
+        'options controlling physical viscosity',
+    ],
+    'idrag': [
+        1,
+        'gas/dust drag (0=off,1=Epstein/Stokes,2=const K,3=const ts)',
+        'options controlling dust',
+    ],
+    'grainsize': [0.100, 'Grain size in cm', 'options controlling dust'],
+    'graindens': [
+        3.000,
+        'Intrinsic grain density in g/cm^3',
+        'options controlling dust',
+    ],
+    'K_code': [
+        1.000,
+        'drag constant when constant drag is used',
+        'options controlling dust',
+    ],
+    'icut_backreaction': [
+        0,
+        'cut the drag on the gas phase (0=no, 1=yes)',
+        'options controlling dust',
+    ],
 }
--- phantomconfig/tests/test_phantomconfig.py
+++ phantomconfig/tests/test_phantomconfig.py
@@ -9,7 +9,9 @@ import phantomconfig as pc
 
 from .stub import test_data
 
-test_file = pathlib.Path(__file__).parent / 'stub' / 'config.in'
+test_phantom_file = pathlib.Path(__file__).parent / 'stub' / 'config.in'
+test_json_file = pathlib.Path(__file__).parent / 'stub' / 'config.json'
+test_dict = test_data.test_dict
 
 
 class TestReadPhantom(unittest.TestCase):
@@ -17,9 +19,32 @@ class TestReadPhantom(unittest.TestCase):
 
     def test_read_phantom_config(self):
 
-        conf = pc.read_config(test_file)
+        conf = pc.read_config(test_phantom_file)
         self.assertEqual(conf.config, test_data.test_config)
         self.assertEqual(conf.header, test_data.test_header)
+        self.assertEqual(conf.datetime, test_data.test_datetime)
+
+
+class TestReadJSON(unittest.TestCase):
+    """Test reading JSON config files."""
+
+    def test_read_json_config(self):
+
+        conf = pc.read_json(test_json_file)
+        self.assertEqual(conf.config, test_data.test_config)
+        # self.assertEqual(conf.header, test_data.test_header)
+        # self.assertEqual(conf.datetime, test_data.test_datetime)
+
+
+class TestReadDict(unittest.TestCase):
+    """Test reading Python dictionaries."""
+
+    def test_read_dict(self):
+
+        conf = pc.read_dict(test_dict)
+        self.assertEqual(conf.config, test_data.test_config)
+        self.assertEqual(conf.header, test_data.test_header)
+        self.assertEqual(conf.datetime, test_data.test_datetime)
 
 
 if __name__ == '__main__':
