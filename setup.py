e261a274a79c640aac961e07b8515e4e0d90ab95
Daniel Mentiplay <d.mentiplay@gmail.com>
2020-05-21 10:13:01 +1000

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -11,6 +11,9 @@ version = re.search(
     io.open('phantomconfig/__init__.py', encoding='utf_8_sig').read(),
 ).group(1)
 
+description = (
+    'phantom-config: parse, convert, modify, and generate Phantom config files'
+)
 long_description = (pathlib.Path(__file__).parent / 'README.md').read_text()
 
 setup(
@@ -20,9 +23,16 @@ setup(
     packages=['phantomconfig'],
     url='http://github.com/dmentipl/phantom-config',
     license='MIT',
-    description='Phantom config files: parse, convert, modify, and generate.',
+    description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=['tomlkit'],
     python_requires='>=3.7',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
 )
